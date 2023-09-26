918db7b2044669c30a9995a051c85959401700c0
ometman <revomet@gmail.com>
2023-09-26 14:30:34 +0000

    Implement navigation and route protection for login

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -5,20 +5,30 @@ import About from '@/routes/About';
 import Login from '@/routes/Login';
 import Profile from '@/routes/Profile';
 import NotMatch from '@/routes/NotMatch';
-import Navbar from './components/Navbar';
+import Layout from '@/components/Layout';
+import SinglePage from '@/routes/SinglePage';
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 const TodoApp = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="*" element={<NotMatch />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 };
 export default TodoApp;
